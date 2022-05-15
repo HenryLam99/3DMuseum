92fe931725aa4c0947f5b380b6bca0184a298873
HenryLam99 <96164048+HenryLam99@users.noreply.github.com>
2022-05-15 16:29:19 +0700

    final

---
+++ js/search.js
@@ -0,0 +1,245 @@
+const data = [
+    {
+        id: 0,
+        name: 'Coliseo de Roma / Roman Colosseum by Néstor F. Marqués on Sketchfab',
+        html: `<div class="sketchfab-embed-wrapper"> 
+                <iframe title="Coliseo de Roma / Roman Colosseum" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="360" height="325" src="https://sketchfab.com/models/544c64b6445e4899a17350c949b7766a/embed"> 
+                </iframe> 
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> 
+                    <a 
+                    href="https://sketchfab.com/3d-models/coliseo-de-roma-roman-colosseum-544c64b6445e4899a17350c949b7766a?utm_medium=embed&utm_campaign=share-popup&utm_content=544c64b6445e4899a17350c949b7766a" 
+                    target="_blank" style="font-weight: bold; color: #1CAAD9;"> Coliseo de Roma / Roman Colosseum 
+                    </a> by <a href="https://sketchfab.com/NestorMarques?utm_medium=embed&utm_campaign=share-popup&utm_content=544c64b6445e4899a17350c949b7766a" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Néstor F. Marqués </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=544c64b6445e4899a17350c949b7766a" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a>
+                </p>
+            </div>`,
+    },
+    {
+        id: 1,
+        name: 'Jade Horned Mythical Creatures 青玉圓雕獨角神獸 by Ancient Jades on Sketchfab',
+        html: ` <div class="sketchfab-embed-wrapper"> 
+                    <iframe title="Jade Horned Mythical Creatures 青玉圓雕獨角神獸" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share width="360" height="325" src="https://sketchfab.com/models/aae987b3eb004d12b3d35a17b4832688/embed"> 
+                    </iframe> 
+                    <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> 
+                        <a href="https://sketchfab.com/3d-models/jade-horned-mythical-creatures-aae987b3eb004d12b3d35a17b4832688?utm_medium=embed&utm_campaign=share-popup&utm_content=aae987b3eb004d12b3d35a17b4832688" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Jade Horned Mythical Creatures 青玉圓雕獨角神獸 
+                        </a> by 
+                        <a href="https://sketchfab.com/antique?utm_medium=embed&utm_campaign=share-popup&utm_content=aae987b3eb004d12b3d35a17b4832688" target="_blank" style="font-weight: bold; color: #1CAAD9;"> Ancient Jades </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=aae987b3eb004d12b3d35a17b4832688" target="_blank" style="font-weight: bold; color: #1CAAD9;">Sketchfab
+                        </a>
+                        </p>
+                </div>`,
+    },
+    {
+        id: 2,
+        name: 'Bia tiến sĩ năm 1442 ( Văn Miếu Quốc Tử Giám) by Di sản văn hóa Việt Nam on Sketchfab',
+        html: `<div class="sketchfab-embed-wrapper">
+                <iframe
+                    title="Bia tiến sĩ năm 1442 ( Văn Miếu Quốc Tử Giám)"
+                    frameborder="0"
+                    allowfullscreen
+                    mozallowfullscreen="true"
+                    webkitallowfullscreen="true"
+                    allow="autoplay; fullscreen; xr-spatial-tracking"
+                    xr-spatial-tracking
+                    execution-while-out-of-viewport
+                    execution-while-not-rendered
+                    web-share
+                    width="360"
+                    height="325"
+                    src="https://sketchfab.com/models/7aac2b6d65764a519fdbc71ceb03227f/embed"
+                >
+                </iframe>
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4a4a4a">
+                    <a
+                        href="https://sketchfab.com/3d-models/bia-tien-si-nam-1442-van-mieu-quoc-tu-giam-7aac2b6d65764a519fdbc71ceb03227f?utm_medium=embed&utm_campaign=share-popup&utm_content=7aac2b6d65764a519fdbc71ceb03227f"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        Bia tiến sĩ năm 1442 ( Văn Miếu Quốc Tử Giám)
+                    </a>
+                    by
+                    <a
+                        href="https://sketchfab.com/dd.design?utm_medium=embed&utm_campaign=share-popup&utm_content=7aac2b6d65764a519fdbc71ceb03227f"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        Di sản văn hóa Việt Nam
+                    </a>
+                    on
+                    <a
+                        href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=7aac2b6d65764a519fdbc71ceb03227f"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                        >Sketchfab</a
+                    >
+                </p>
+            </div>`,
+    },
+    {
+        id: 3,
+        name: 'St Thomas - San Giovanni in Laterano, Rome by egiptologo91 on Sketchfab',
+        html: `<div class="sketchfab-embed-wrapper">
+                <iframe
+                    title="St Thomas - San Giovanni in Laterano, Rome"
+                    frameborder="0"
+                    allowfullscreen
+                    mozallowfullscreen="true"
+                    webkitallowfullscreen="true"
+                    allow="autoplay; fullscreen; xr-spatial-tracking"
+                    xr-spatial-tracking
+                    execution-while-out-of-viewport
+                    execution-while-not-rendered
+                    web-share
+                    width="360"
+                    height="325"
+                    src="https://sketchfab.com/models/743c30e596604aff8d724dd3c62a6e88/embed"
+                >
+                </iframe>
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4a4a4a">
+                    <a
+                        href="https://sketchfab.com/3d-models/st-thomas-san-giovanni-in-laterano-rome-743c30e596604aff8d724dd3c62a6e88?utm_medium=embed&utm_campaign=share-popup&utm_content=743c30e596604aff8d724dd3c62a6e88"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        St Thomas - San Giovanni in Laterano, Rome
+                    </a>
+                    by
+                    <a
+                        href="https://sketchfab.com/egiptologo91?utm_medium=embed&utm_campaign=share-popup&utm_content=743c30e596604aff8d724dd3c62a6e88"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        egiptologo91
+                    </a>
+                    on
+                    <a
+                        href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=743c30e596604aff8d724dd3c62a6e88"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                        >Sketchfab</a
+                    >
+                </p>
+            </div>`,
+    },
+    {
+        id: 4,
+        name: 'trong dong ngoc lu by 4T on Sketchfab',
+        html: `<div class="sketchfab-embed-wrapper">
+                <iframe
+                    title="trong dong ngoc lu"
+                    frameborder="0"
+                    allowfullscreen
+                    mozallowfullscreen="true"
+                    webkitallowfullscreen="true"
+                    allow="autoplay; fullscreen; xr-spatial-tracking"
+                    xr-spatial-tracking
+                    execution-while-out-of-viewport
+                    execution-while-not-rendered
+                    web-share
+                    width="360"
+                    height="325"
+                    src="https://sketchfab.com/models/91d08901223343d98cdcd847d7a7953a/embed"
+                >
+                </iframe>
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4a4a4a">
+                    <a
+                        href="https://sketchfab.com/3d-models/trong-dong-ngoc-lu-91d08901223343d98cdcd847d7a7953a?utm_medium=embed&utm_campaign=share-popup&utm_content=91d08901223343d98cdcd847d7a7953a"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        trong dong ngoc lu
+                    </a>
+                    by
+                    <a
+                        href="https://sketchfab.com/tranduc95tb?utm_medium=embed&utm_campaign=share-popup&utm_content=91d08901223343d98cdcd847d7a7953a"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        4T
+                    </a>
+                    on
+                    <a
+                        href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=91d08901223343d98cdcd847d7a7953a"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                        >Sketchfab</a
+                    >
+                </p>
+            </div>`,
+    },
+    {
+        id: 5,
+        name: 'Christ the Redeemer, Rio de Janeiro, Brazil by Brian Trepanier on Sketchfab',
+        html: `<div class="sketchfab-embed-wrapper">
+                <iframe
+                    title="Christ the Redeemer, Rio de Janeiro, Brazil"
+                    frameborder="0"
+                    allowfullscreen
+                    mozallowfullscreen="true"
+                    webkitallowfullscreen="true"
+                    allow="autoplay; fullscreen; xr-spatial-tracking"
+                    xr-spatial-tracking
+                    execution-while-out-of-viewport
+                    execution-while-not-rendered
+                    web-share
+                    width="360"
+                    height="325"
+                    src="https://sketchfab.com/models/90d53b3cb4b042ce845a24baa679109e/embed"
+                >
+                </iframe>
+                <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4a4a4a">
+                    <a
+                        href="https://sketchfab.com/3d-models/christ-the-redeemer-rio-de-janeiro-brazil-90d53b3cb4b042ce845a24baa679109e?utm_medium=embed&utm_campaign=share-popup&utm_content=90d53b3cb4b042ce845a24baa679109e"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        Christ the Redeemer, Rio de Janeiro, Brazil
+                    </a>
+                    by
+                    <a
+                        href="https://sketchfab.com/CMBC?utm_medium=embed&utm_campaign=share-popup&utm_content=90d53b3cb4b042ce845a24baa679109e"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                    >
+                        Brian Trepanier
+                    </a>
+                    on
+                    <a
+                        href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=90d53b3cb4b042ce845a24baa679109e"
+                        target="_blank"
+                        style="font-weight: bold; color: #1caad9"
+                        >Sketchfab</a
+                    >
+                </p>
+            </div>`,
+    },
+];
+
+const searchInput = document.querySelector('.search-box input');
+const searchDropdown = document.querySelector('.search-dropdown');
+
+const searchData = (searchValue) => {
+    const searchResults = data.filter((search) => {
+        return search.name.toLowerCase().includes(searchValue.toLowerCase());
+    });
+
+    if (searchResults.length) {
+        searchDropdown.innerHTML = '';
+        searchResults.forEach((search) => {
+            const searchItem = document.createElement('li');
+            searchItem.classList.add('search-item');
+            searchItem.innerHTML = search.html;
+            searchDropdown.appendChild(searchItem);
+        });
+    } else {
+        searchDropdown.innerHTML = '<div class="search-error">No results found</div>';
+    }
+};
+
+const searchHandler = () => {
+    const searchValue = searchInput.value;
+    if (!searchValue) return searchDropdown.classList.add('hidden');
+
+    searchData(searchValue);
+    searchDropdown.classList.remove('hidden');
+};
+
+searchInput.addEventListener('input', searchHandler);
